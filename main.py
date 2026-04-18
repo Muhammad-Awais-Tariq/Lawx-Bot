50f3f1350f6d7f450dc2463ee935f914b79327d6
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-04-18 14:33:01 +0500

    added functionality for banning person

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 import logging
+import typing
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -11,12 +12,22 @@ handler = logging.FileHandler(filename="Discord.log" , encoding="utf-8" , mode="
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-intents.presences = True #
+intents.presences = True 
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print("ready")
+
+@bot.command()
+async def ban(ctx, members: commands.Greedy[discord.Member],
+                   delete_days: typing.Optional[int] = 0, *,
+                   reason: str):
+    delete_seconds = delete_days * 86400 
+    for member in members:
+        await member.send(f"you have been banned for following reason {reason}")        
+        await member.ban(delete_message_seconds=delete_seconds, reason=reason)
+
 
 bot.run(token,log_handler=handler,log_level=logging.DEBUG)
