a7436bdea5c76a1ece28113038bea8b76e7b603f
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-04-18 14:08:49 +0500

    basic setup done

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+import logging
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename="Discord.log" , encoding="utf-8" , mode="w")
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.presences = True #
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("ready")
+
+bot.run(token,log_handler=handler,log_level=logging.DEBUG)
